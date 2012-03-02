169c3f9bee42a4a27f970306b248fb361f81572b
suer <suetsugu.r@gmail.com>
2012-03-03 00:34:53 +0900

    refactor spec refs #1044

--- spec/redbrew/cli_spec.rb
+++ spec/redbrew/cli_spec.rb
@@ -5,8 +5,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Redbrew::CLI do
   describe 'version' do
-    it do
-      capture(:stdout) { Redbrew::CLI.new(['version']).run }.should == "Redbrew #{Redbrew::VERSION}\n"
-    end
+    subject { capture(:stdout) { Redbrew::CLI.new(['version']).run } }
+    it { should == "Redbrew #{Redbrew::VERSION}\n" }
   end
 end
