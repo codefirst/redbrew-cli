60ebf623e1e7c82bd1504c01072f74881ac79cba
suer <suetsugu.r@gmail.com>
2012-03-03 00:35:02 +0900

    refactor spec refs #1045

--- spec/redbrew/redbrew_web_spec.rb
+++ spec/redbrew/redbrew_web_spec.rb
@@ -4,26 +4,26 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Redbrew::RedbrewWeb do
-  RESPONSE = <<JSON
-{
-  "name": "RedmineAttachmentsSize",
-  "created_at": "2012-02-28T13:56:48Z",
-  "create_user_id": 1,
-  "updated_at": "2012-02-28T13:56:48Z",
-  "url": "https://github.com/suer/redmine_attachments_size",
-  "id": 1,
-  "formula": "formula",
-  "desription": "Disk Usage Plugin",
-  "update_user_id": 1
-}
-JSON
+  RESPONSE = <<-JSON
+  {
+    "name": "RedmineAttachmentsSize",
+    "created_at": "2012-02-28T13:56:48Z",
+    "create_user_id": 1,
+    "updated_at": "2012-02-28T13:56:48Z",
+    "url": "https://github.com/suer/redmine_attachments_size",
+    "id": 1,
+    "formula": "formula",
+    "desription": "Disk Usage Plugin",
+    "update_user_id": 1
+  }
+  JSON
 
   FakeWeb.register_uri(:get, 'http://redbrew.heroku.com/plugins/RedmineAttachmentsSize.json', :body => RESPONSE)
 
-  describe 'plugin' do
-    it do
-      Redbrew::RedbrewWeb.plugin('RedmineAttachmentsSize')['name'].should == 'RedmineAttachmentsSize'
-    end
+  context 'get plugin' do
+    subject { Redbrew::RedbrewWeb.plugin('RedmineAttachmentsSize') }
+    its(['name']) { should == 'RedmineAttachmentsSize' }
+    its(['create_user_id']) { should == 1 }
   end
 end
 
