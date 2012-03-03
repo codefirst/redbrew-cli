88e915c3c2c34e637240bf038967d96c8e22f3ca
suer <suetsugu.r@gmail.com>
2012-03-03 20:05:44 +0900

    spec for raise error #1045

--- spec/redbrew/fetcher/fetcher_spec.rb
+++ spec/redbrew/fetcher/fetcher_spec.rb
@@ -4,9 +4,11 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Redbrew::Fetcher do
-#  describe 'fetch' do
-#    lambda{ Redbrew::Fetcher.new('', '').fetch }.should raise_error
-#  end
+  describe 'fetch' do
+    it 'fetch error' do
+      lambda{ Redbrew::Fetcher.new('', '').fetch }.should raise_error
+    end
+  end
 
   describe 'create' do
     subject { Redbrew::Fetcher.create('git', '', '')}
